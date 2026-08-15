53041329b5f66a4f8d238316978ca57375543f25
Anitha-2403 <anithagantala843@gmail.com>
2026-08-15 13:33:55 +0530

    Add new allowed origin for CORS

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "https://careercopilot-ai.netlify.app",
+  "https://anitha-2403.github.io",
   "http://127.0.0.1:5500",
   "http://localhost:5500",
 ];
